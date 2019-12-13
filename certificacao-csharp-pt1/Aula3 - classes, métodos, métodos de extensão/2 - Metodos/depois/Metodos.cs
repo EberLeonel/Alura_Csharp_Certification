3d1a6bd7348dd7bfe65be8ae992ddea4631e1310
eber <eber@maisdados.com.br>
2019-12-13 10:24:42 -0300

    First Commit. Scope of study repository with some classes alredy taken.

---
+++ certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/2 - Metodos/depois/Metodos.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class Metodos : IAulaItem
+    {
+        public void Executar()
+        {
+            Retangulo retangulo = new Retangulo(12, 10);
+
+            Retangulo vRetangulo = new Retangulo(10,10);
+            Console.WriteLine(retangulo.Semelhante(retangulo.Largura, vRetangulo.Largura));
+
+            vRetangulo = new Retangulo(5, 6);
+            Console.WriteLine(Retangulo.Semelhante(retangulo, vRetangulo));
+        }
+    }
+
+    class Retangulo
+    {
+        public double Altura { get; set; }
+        public double Largura { get; set; }
+
+        public Retangulo(double altura, double largura)
+        {
+            Altura = altura;
+            Largura = largura;
+
+            Console.WriteLine($"altura: {altura}, largura: {largura}");
+        }
+
+        public double GetArea()
+        {
+            return Altura * Largura;
+        }
+
+        internal static bool Semelhante(Retangulo pRetangulo, Retangulo pOutroRetangulo)
+        {
+            return
+                ((pRetangulo.Largura / pRetangulo.Altura) == /*proporção deste retângulo*/
+                (pOutroRetangulo.Largura / pOutroRetangulo.Largura)) /*proporção do outro retângulo*/
+                ||
+                ((pRetangulo.Altura / pRetangulo.Largura) == /*compara a proporção inversa*/
+                (pOutroRetangulo.Altura / pOutroRetangulo.Altura));
+        }
+
+        internal bool Semelhante(double outroRetanguloAltura, double outroRetanguloLargura)
+        {
+            return
+                ((Largura / Altura) == /*proporção deste retângulo*/
+                (outroRetanguloLargura / outroRetanguloAltura)) /*proporção do outro retângulo*/
+                ||
+                ((Altura / Largura) == /*compara a proporção inversa*/
+                (outroRetanguloLargura / outroRetanguloAltura));
+        }
+    }
+}
---
+++ certificacao-csharp-pt1/Aula3 - classes, métodos, métodos de extensão/3 - Parametros Opcionais/depois/ParametrosNomeados.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class ParametrosNomeados : IAulaItem
+    {
+        public void Executar()
+        {
+            ImprimirDetalhesDoPedido("Geraldin", 132, "Torradeira");
+            ImprimirDetalhesDoPedido(numeroPedido: 31, nomeProduto:"Caneta Azul",vendedor: "Dona Creusa");
+
+            ImprimirDetalhesDoPedido("Maria d Fátima", 31, nomeProduto:"Caneca Vermelha");
+
+
+        }
+
+        void ImprimirDetalhesDoPedido(string vendedor, int numeroPedido, string nomeProduto)
+        {
+            if (string.IsNullOrWhiteSpace(vendedor))
+            {
+                throw new ArgumentException(message: "Nome de vendedor não pode ser nulo ou vazio.", paramName: nameof(vendedor));
+            }
+
+            Console.WriteLine($"Vendedor: {vendedor}, Pedido No.: {numeroPedido}, Produto: {nomeProduto}");
+        }
+    }
+}
