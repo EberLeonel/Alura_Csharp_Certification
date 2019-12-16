1ddce8e8b5034dd46ff389df24224bd55c6339ba
eber <eber@maisdados.com.br>
2019-12-16 15:05:48 -0300

    Overload and Replacement Methods lesson.

---
+++ certificacao-csharp-pt1/Aula4 - criar métodos sobrecarregados e substituídos/1 - Métodos Sobrecarregados/depois/Sobrecargas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class Sobrecargas : IAulaItem
+    {
+        public void Executar()
+        {
+            //VOLUME DO CUBO = lado ^ 3;
+            int lado = 3;
+            Console.WriteLine($"Volume do Cubo: {Volume(lado)}");
+
+            //VOLUME DO CILINDRO = altura * PI * raio ^ 2;
+            double raio = 2;
+            int altura = 10;
+            Console.WriteLine($"Volume do cilindro: {Volume(altura, raio)}");
+
+            //VOLUME DO PRISMA = largura * profundidade * altura
+            long largura = 10;
+            altura = 6;
+            int profundidade = 4;
+            Console.WriteLine($"Volume prisma: {Volume(largura, profundidade, altura)}");
+        }
+        double Volume(double pLado)
+        {
+            return Math.Pow(pLado, 3);
+        }
+        double Volume(double pAltura, double pRaio)
+        {
+            return pAltura * Math.PI * Math.Pow(pRaio, 2);
+        }
+        double Volume(double pLargura, double pProfundidade, double pAltura)
+        {
+            return pLargura * pProfundidade * pAltura;
+        }
+    }
+}
---
+++ certificacao-csharp-pt1/Aula4 - criar métodos sobrecarregados e substituídos/2 - Métodos Substituídos/depois/2 - Métodos Substituídos.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class MetodosSubstituidos : IAulaItem
+    {
+        public void Executar()
+        {
+            Animal gato = new Gato() { Nome = "Bichano" };
+            gato.Beber();
+            gato.Comer();
+            gato.Andar();
+
+            Gato gata = new Gato() { Nome = "Penelope" };
+            gato.Beber();
+            gato.Comer();
+            gato.Andar();
+        }
+    }
+
+    class Animal
+    {
+        public String Nome { get; set; }
+
+        public void Beber()
+        {
+            Console.WriteLine("Animal.Beber");
+        }
+
+        public virtual void Comer()
+        {
+            Console.WriteLine("Animal.Comer");
+        }
+
+        public virtual void Andar()
+        {
+            Console.WriteLine("Animal.Andar");
+        }
+    }
+
+    class Gato : Animal
+    {
+        public new void Beber()
+        {
+            Console.WriteLine("Gato.Beber");
+        }
+
+        public override void Comer()
+        {
+            Console.WriteLine("Gato.Comer");
+        }
+
+        public override void Andar()
+        {
+            Console.WriteLine("Gato.Andar");
+        }
+    }
+}
